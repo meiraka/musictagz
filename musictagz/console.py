997009e58236ba6e282eb072b9c7fb5691b4ccf4
meiraka <meiraka.pre@gmail.com>
2016-08-28 23:04:32 +0900

    read/write stdin/stdout instead of tracks.yml

--- musictagz/console.py
+++ musictagz/console.py
@@ -8,8 +8,6 @@ import yaml
 
 from musictagz import tags
 
-DEFAULT_PATH = './tracks.yml'
-
 
 def main(argv=None):
     if argv is None:
@@ -20,25 +18,24 @@ def main(argv=None):
     parser.add_argument('-l', '--load', action='store_false', dest='dump')
     ret = parser.parse_args(argv)
     if ret.dump:
-        dump_yaml('./*', DEFAULT_PATH)
+        dump_yaml('./*')
         return 0
     else:
-        load_yaml(DEFAULT_PATH)
+        load_yaml()
         return 0
 
 
-def dump_yaml(glob_path, yaml_path):
+def dump_yaml(glob_path):
     flatten_data = tags.read(glob_path)
     data = tags.deflatten(flatten_data)
     out = yaml.safe_dump(data, default_flow_style=False,
                          allow_unicode=True, encoding='utf8')
-    with open(yaml_path, 'w') as f:
-        f.write(out)
+    sys.stdout.write(out)
+    return 0
 
 
-def load_yaml(yaml_path):
-    with open(yaml_path, 'r') as f:
-        data = yaml.safe_load(f.read())
+def load_yaml():
+    data = yaml.safe_load(sys.stdin.read())
 
     def u(s):
         if type(s) == str:
@@ -54,3 +51,4 @@ def load_yaml(yaml_path):
 
     flatten_data = tags.flatten(data)
     tags.write(flatten_data)
+    return 0
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,10 +1,12 @@
+import sys
+
 from musictagz import console
+from musictagz import tags
 
 
 def test_main_dump(monkeypatch):
-    def mock_dump_yaml(glob_path, yaml_path):
+    def mock_dump_yaml(glob_path):
         assert './*' == glob_path
-        assert './tracks.yml' == yaml_path
 
     monkeypatch.setattr(console, 'dump_yaml', mock_dump_yaml)
 
@@ -13,11 +15,60 @@ def test_main_dump(monkeypatch):
 
 
 def test_main_load(monkeypatch):
-    def mock_load_yaml(yaml_path):
-        assert './tracks.yml' == yaml_path
+    def mock_load_yaml():
+        pass
 
     monkeypatch.setattr(console, 'load_yaml', mock_load_yaml)
 
     assert 0 == console.main(['--load'])
     assert 0 == console.main(['-l'])
     assert 0 == console.main([])
+
+
+def test_dump_yaml(monkeypatch):
+    def mock_tags_read(glob_path):
+        assert './*' == glob_path
+        return {'01.flac': {'plain': {'TEST': 'TEST'}}}
+
+    def mock_tags_deflatten(flatten_data):
+        assert {'01.flac': {'plain': {'TEST': 'TEST'}}} == flatten_data
+        return {'*.flac': {'plain': {'TEST': 'TEST'}}}
+
+    class MockStdout(object):
+        def __init__(self):
+            pass
+
+        def write(self, data):
+            assert ''''*.flac':
+  plain:
+    TEST: TEST
+''' == data
+
+    monkeypatch.setattr(tags, 'read', mock_tags_read)
+    monkeypatch.setattr(tags, 'deflatten', mock_tags_deflatten)
+    monkeypatch.setattr(sys, 'stdout', MockStdout())
+    assert 0 == console.dump_yaml('./*')
+
+
+def test_load_yaml(monkeypatch):
+    def mock_tags_flatten(data):
+        assert {'*.flac': {'plain': {'TEST': 'TEST'}}} == data
+        return {'01.flac': {'plain': {'TEST': 'TEST'}}}
+
+    def mock_tags_write(flatten_data):
+        assert {'01.flac': {'plain': {'TEST': 'TEST'}}} == flatten_data
+
+    class MockStdin(object):
+        def __init__(self):
+            pass
+
+        def read(self):
+            return ''''*.flac':
+  plain:
+    TEST: TEST
+'''
+
+    monkeypatch.setattr(tags, 'write', mock_tags_write)
+    monkeypatch.setattr(tags, 'flatten', mock_tags_flatten)
+    monkeypatch.setattr(sys, 'stdin', MockStdin())
+    assert 0 == console.load_yaml()
